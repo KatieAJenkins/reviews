89fe7049dd97d2b67fd9a4a57611e12afa549cb0
KatieAJenkins <kind2karma@gmail.com>
2016-12-30 22:46:29 -0700

    more higher order functions

--- functions.js
+++ functions.js
@@ -20,12 +20,11 @@ console.log(double(9));
 var waffle = double; //move function around
 console.log(waffle(30));
 
-//CALLBACK FUNCTIONS
-
-//use callback function to filter to find dogs
-//filter will loop through each item in the array
+//filter
+//will loop through each item in the array
 //will pass in each item to the call back function (animal)
-//expects callback function to return true or false to tell filter if item should be in new array
+//expects callback function to return true or false
+//if true, put in new array. false, dump it
 //when done will return new filtered array
 var animals = [
   { name: "Sake" , species: 'dog'},
@@ -37,6 +36,23 @@ var animals = [
 
 var dogs = animals.filter(function (animal) {
   return animal.species === 'dog';
-})
+});
 
 console.log(dogs);
+
+//map --> applies a method on each item in array
+var array = [1, 4, 9];
+var roots = array.map(Math.sqrt);
+// console.log(roots);
+
+//reduce --> returns 1 thing
+var array = [5, 2];
+
+var reduced = array.reduce(sum);
+  function sum (total , num) {
+    console.log(total);
+    console.log(num);
+    return total + num;
+  };
+
+  console.log(reduced);
