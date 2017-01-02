07535b9c2dd5e0345d4fc70f6ff7fe169c53b333
KatieAJenkins <kind2karma@gmail.com>
2017-01-01 21:21:44 -0700

    for in loops objects

--- objects.js
+++ objects.js
@@ -3,6 +3,22 @@
 var capitals = {
   Colorado: 'Denver',
   Nebraska: 'Lincoln'
-};
+}
 
-console.log("The capital of Colorado is " + capitals.Colorado);
+console.log("The capital of Colorado is " + capitals.Colorado); //dot notation
+console.log("The capital of Nebraska is "  + capitals["Nebraska"]);//bracket
+
+//iterate through Object FOR/IN loop
+//for (variableName -->variable to store name of property in ourObject)
+// for (var state in capitals) {
+//   console.log("The capital of " + state + " is " + capitals.state);
+// }
+
+for (var key in capitals) { //loop once for each key value in object
+  console.log('Key: ' + key + 'Value: ' + capitals[key]);
+}
+
+//can name key variable anything!
+for (var state in capitals) {
+  console.log('Key: ' + state + 'Value: ' + capitals[state]);
+}
